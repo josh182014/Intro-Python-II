503b95dba76647dff494e60c1ce3fd3ecd0ea47c
josh182014 <josh182014@gmail.com>
2019-09-04 16:42:37 -0600

    got rid of test code

--- src/adv.py
+++ src/adv.py
@@ -40,8 +40,7 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 player = Player(room['outside'])
-def test:
-    hi
+
 
 # Write a loop that:
 #
